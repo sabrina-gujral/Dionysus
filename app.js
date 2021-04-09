34e53ae706bfcaac0bfc9a0588c26086b52c255e
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-04-10 04:05:50 +0530

    Add movie recommendation system website

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const https = require("https");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+app.post("/", function (req, res) {
+  const name = req.body.name;
+  const userId = req.body.userid;
+
+  res.redirect("/rec/" + userId + "/" + name);
+});
+
+app.get("/rec/:userId/:name", function (req, res) {
+  const userId = req.params.userId;
+  const name = req.params.name;
+  const url = "https://still-dusk-52410.herokuapp.com/movies/" + userId + "/re";
+
+  https.get(url, function (response) {
+    var data;
+    response.on("data", function (chunk) {
+      if (!data) {
+        data = chunk;
+      } else {
+        data += chunk;
+      }
+    });
+
+    response.on("end", function () {
+      const movieRec = JSON.parse(data);
+      movieRec.forEach(i => i.generes = i.genres.split("|"));
+      res.render('list', {rec: movieRec, name: name, userId: userId});
+    });
+  });
+});
+
+app.post('/rec/:userId/:name', function(req, res){
+    const userId = req.params.userId;
+    const name = req.params.name;
+    const newId = req.body.userid;
+    
+    res.redirect('/rec/' + newId + '/' + name);
+})
+
+app.listen(3000, function () {
+  console.log("listening on port 3000...");
+});
