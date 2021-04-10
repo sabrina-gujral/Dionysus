a23128a6273d61f05e94fb84e8a6b1aef7db11ae
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-04-11 02:52:43 +0530

    Clean and format </>

--- app.js
+++ app.js
@@ -11,13 +11,35 @@ app.get("/", function (req, res) {
   res.render("index");
 });
 
+const genresList = [
+  "Animation",
+  "Children",
+  "Comedy",
+  "Musical",
+  "Horror",
+  "Drama",
+  "Romance",
+  "Action",
+  "Crime",
+  "Sci-fi",
+  "Thriller",
+  "Adventure",
+  "Fantasy",
+  "Mystery",
+  "War",
+  "Western",
+  "Documentary",
+];
+
+genresList.sort();
+
 app.post("/", function (req, res) {
   const name = req.body.name;
   const userId = req.body.userid;
 
-  if(userId < 0 || userId > 610){
-      res.render('404');
-  } else{
+  if (userId < 0 || userId > 610) {
+    res.render("404");
+  } else {
     res.redirect("/rec/" + userId + "/" + name);
   }
 });
@@ -40,22 +62,27 @@ app.get("/rec/:userId/:name", function (req, res) {
     response.on("end", function () {
       const movieRec = JSON.parse(data);
       const genres = [];
-      movieRec.forEach(i => genres.push(i.genres.split("|")));
-      res.render('list', {rec: movieRec, name: name, userId: userId});
+      movieRec.forEach((i) => genres.push(i.genres.split("|")));
+      res.render("list", {
+        rec: movieRec,
+        name: name,
+        userId: userId,
+        genres: genresList,
+      });
     });
   });
 });
 
-app.post('/rec/:userId/:name', function(req, res){
-    const name = req.params.name;
-    const newId = req.body.userid;
+app.post("/rec/:userId/:name", function (req, res) {
+  const name = req.params.name;
+  const newId = req.body.userid;
 
-    if(newId < 0 || newId > 610){
-        res.render('404');
-    } else {
-    res.redirect('/rec/' + newId + '/' + name);
-    }
-})
+  if (newId < 0 || newId > 610) {
+    res.render("404");
+  } else {
+    res.redirect("/rec/" + newId + "/" + name);
+  }
+});
 
 app.listen(3000, function () {
   console.log("listening on port 3000...");
