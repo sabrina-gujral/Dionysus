8689a29c0f1f3fd18a57e300b96695e5cf17a87b
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-04-10 22:09:01 +0530

    Add 404 page and alter some styling

--- app.js
+++ app.js
@@ -15,7 +15,11 @@ app.post("/", function (req, res) {
   const name = req.body.name;
   const userId = req.body.userid;
 
-  res.redirect("/rec/" + userId + "/" + name);
+  if(userId < 0 || userId > 610){
+      res.render('404');
+  } else{
+    res.redirect("/rec/" + userId + "/" + name);
+  }
 });
 
 app.get("/rec/:userId/:name", function (req, res) {
@@ -35,18 +39,22 @@ app.get("/rec/:userId/:name", function (req, res) {
 
     response.on("end", function () {
       const movieRec = JSON.parse(data);
-      movieRec.forEach(i => i.generes = i.genres.split("|"));
+      const genres = [];
+      movieRec.forEach(i => genres.push(i.genres.split("|")));
       res.render('list', {rec: movieRec, name: name, userId: userId});
     });
   });
 });
 
 app.post('/rec/:userId/:name', function(req, res){
-    const userId = req.params.userId;
     const name = req.params.name;
     const newId = req.body.userid;
-    
+
+    if(newId < 0 || newId > 610){
+        res.render('404');
+    } else {
     res.redirect('/rec/' + newId + '/' + name);
+    }
 })
 
 app.listen(3000, function () {
