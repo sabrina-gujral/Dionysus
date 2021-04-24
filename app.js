bc38be760a1c470dac6e5920289dd9d7c94c8f4a
Sabrina Gujral <sabrinagujral26@gmail.com>
2021-04-25 04:52:16 +0530

    Add some styling changes

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const https = require("https");
-const spawn = require("child_process").spawn;
 const app = express();
 
 app.set("view engine", "ejs");
@@ -102,6 +101,8 @@ app.post("/rec/:userId/:name/:filters?/:title?", function (req, res) {
 });
 
 app.get("/:title", function (req, res) {
+  const spawn = require("child_process").spawn;
+
   const title = req.params.title;
   const pythonProcess = spawn("python", ["./recommender/recommend.py", title]);
 
